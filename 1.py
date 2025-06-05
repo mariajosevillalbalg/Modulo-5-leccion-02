424716cfc1aa1008cc4fad0f4ba609f4a339d480
mariajosevillalbalg <mjvlalgorithmic@gmail.com>
2025-06-05 18:40:57 -0500

    clase 1

---
+++ 1.py
@@ -0,0 +1,54 @@
+from pygame import *
+' ' 'Clases requeridas' ' '
+
+#clase padre para los objetos
+class GameSprite(sprite.Sprite):
+    #Constructor de clase
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+ 
+        #Cada objeto debe almacenar la propiedad image
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+ 
+        #cada objeto debe tener la propiedad rect - el rectángulo en el que se encuentra
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#Escena del juego:
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+
+#Personajes del juego:
+packman = GameSprite('hero.png', 5, win_height - 80, 4)
+monster = GameSprite('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+
+game = True
+clock = time.Clock()
+FPS = 60
+
+#música
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer.music.play()
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    window.blit(background,(0, 0))
+    packman.reset()
+    monster.reset()
+
+    display.update()
+    clock.tick(FPS)
+
